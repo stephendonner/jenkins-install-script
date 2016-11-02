e81c20dda9db5e39ec2e7c1ed58457982c5d9fab
Stephen Donner <stephen.donner@gmail.com>
2016-11-02 05:05:26 -0700

    Fix typo in wget option

--- configure-jenkins.sh
+++ configure-jenkins.sh
@@ -8,12 +8,14 @@ sudo yum -y install git
 sudo yum install java-1.8.0-openjdk.x86_64
 
 # install Jenkins LTS
-sudo wget -0 /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins-ci.org.key
 sudo yum install jenkins
+
+# Options to pass to java when running Jenkins.
+JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Dorg.apache.commons.jelly.tags.fmt.timeZone=America/Los_Angeles -Djenkins.security.FrameOptionsPageDecorator.enabled=false -Djenkins.install.runSetupWizard=false"
 
 sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
 
-# Options to pass to java when running Jenkins.
-JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Dorg.apache.commons.jelly.tags.fmt.timeZone=America/Los_Angeles -Djenkins.security.FrameOptionsPageDecorator.enabled=false -Djenkins.install.runSetupWizard=false"
+sudo systemctl restart jenkins.service
