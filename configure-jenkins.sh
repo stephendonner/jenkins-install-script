006302a41daf453ffd18d58b5a4573754d9ffee0
Stephen Donner <stephen.donner@gmail.com>
2016-11-02 04:17:46 -0700

    Initial commit of Jenkins-configure script

---
+++ configure-jenkins.sh
@@ -0,0 +1,13 @@
+# install Java
+sudo yum install java-1.8.0-openjdk.x86_64
+
+# install Jenkins LTS
+sudo wget -0 /etc/yum.repos.d/jenkins.repo http://pkg.jenkins.io/redhat-stable/jenkins.repo
+sudo rpm --import http://pkg.jenkins.io/redhat-stable/jenkins-ci.org.key
+sudo yum install jenkins
+
+sudo systemctl start jenkins.service
+sudo systemctl enable jenkins.service
+
+# Options to pass to java when running Jenkins.
+JENKINS_JAVA_OPTIONS="-Djava.awt.headless=true -Dorg.apache.commons.jelly.tags.fmt.timeZone=America/Los_Angeles -Djenkins.security.FrameOptionsPageDecorator.enabled=false"
